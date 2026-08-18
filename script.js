6607c2a5bb9f90a9b6575b8d2b08839ad67148cd
Cristian Cabañas <cristhianbenegas1111@gmail.com>
2026-08-18 11:32:07 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -185,3 +185,11 @@ if (menuToggle && menuPrincipal) {
 
     });
 }
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('nav');
+
+if (menuToggle && nav) {
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
