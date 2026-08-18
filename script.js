e74773ac2bad38f1ed9dfa2870023a60c693b180
Cristian Cabañas <cristhianbenegas1111@gmail.com>
2026-08-17 21:48:29 -0300

    Refactor preloader and metrics animation logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => 
+document.addEventListener('DOMContentLoaded', () => {
     
     const preloader = document.getElementById('preloader');
     const loaderBar = document.getElementById('loaderBar');
